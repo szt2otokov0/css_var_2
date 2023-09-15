b53fabb063706a63e5dac6bb72690e545a9a07ee
szt2otokov <szt2otokov@vasvari.org>
2023-09-15 08:46:35 +0200

    tasks 3 dun

--- js.js
+++ js.js
@@ -1,15 +1,22 @@
 let root;
 let oldColor;
-let picker;
+let colorpick;
+let numberpick;
 window.onload = () => {
     root = document.querySelector(":root");
     oldColor = getComputedStyle(root).getPropertyValue("--bg-root");
     picker = document.getElementById("colorpicker");
+    numberpick = document.getElementById("numberpicker");
     console.log(oldColor);
     picker.value = oldColor;
 }
 
 function valt() {
-    root.style.setProperty("--bg-root",picker.value)
+    let x = parseInt(numberpick.value);
+    if(x < 1 || x > 8) {
+        alert("nincs ilyen téglalap");
+        return;
+    }
+    root.style.setProperty(`--b${x}`,picker.value)
 }
 
